d87312a31047d73ba37162aeebdb8dc1d35e3224
galadima-cyber <mukhtargaladeema@gmail.com>
2026-05-30 12:00:43 +0100

    kkk

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:5000';
+// const BASE_URL = 'http://localhost:5000';
+const BASE_URL = import.meta.env.VITE_API_URL;
 // const BASE_URL = 'http://192.168.37.1:5173';
 const api = axios.create({ baseURL: BASE_URL, timeout: 120000 });
 
